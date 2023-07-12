53b407f42d123d6b491952e986d9d64299260fe9
Медведев Максим Александрович <medvedevma@rambler.ru>
2023-07-12 14:01:17 +0500

    project_2 added

---
+++ Project_2/my-app/src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import PostList from "./components/PostList";
+import PostCreate from "./components/PostCreate";
+import Home from "./components/Home";
+import styles from '../src/App.module.css';
+
+const App = () => {
+  return (
+    <Router>
+      <div className={styles.container}>
+        <h1 >React Post App</h1>
+        <nav>
+          <div><Link className={styles.navLink} to="/">Home</Link></div>
+          <div><Link className={styles.navLink} to="/posts">Posts</Link></div>
+        </nav>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/posts" element={<PostList />} />
+          <Route exact path="/posts/create" element={<PostCreate />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
